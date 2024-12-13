9529435e349851fa338982bab0c17db33db30dba
Nova. <rajgharsmp@gmail.com>
2024-12-13 08:23:57 +0530

    Update main.py
    Added jishaku

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import discord
 import os
 from discord.ext import commands
 from config.config import color, token, owner
+import jishaku 
 
 class Nova(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix="-", intents=discord.Intents.all(), help_command=None)
-        self.owner_ids = OWNER
+        super().__init__(command_prefix="-", intents=discord.Intents.all(), help_command=None)        
 
     async def on_ready(self):
         print(f'{self.user.display_name} is ready!')
         await self.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="/help"))
 
     async def setup_hook(self):
+        self.load_extension("jishaku") 
+        self.owner_ids = OWNER
         for filename in os.listdir('./cogs'):
             if filename.endswith('.py'):
                 try:
